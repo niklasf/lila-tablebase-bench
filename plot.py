7ad4b917b0098f59fb2d5554310be63e493c9682
root <root@bwrdd.lichess.ovh>
2024-06-23 14:51:15 +0000

    allow filtering piece count

--- plot.py
+++ plot.py
@@ -4,17 +4,22 @@ import sys
 import math
 import statistics
 
-GROUP_MS = 10
+GROUP_MS = 50
 
 def group(ms):
     return ms // GROUP_MS
+
+def piece_count(fen):
+    board, _ = fen.split(" ", 1)
+    return sum((ch in "PNBRQKpnbrqk") for ch in board)
 
 groups = {}
 
 for line in open(sys.argv[1]):
     fen, ms = line.split("\t")
     ms = int(ms)
-    groups[group(ms)] = groups.get(group(ms), 0) + 1
+    if True or piece_count(fen) == 5:
+        groups[group(ms)] = groups.get(group(ms), 0) + 1
 
 log_groups = {i: math.log(v) for i, v in groups.items()}
 
@@ -23,7 +28,7 @@ maximum = max(log_groups.values())
 for i in range(30):
     val = log_groups.get(i, 0)
     width = round(val * 80 / maximum)
-    print(str(i * GROUP_MS).rjust(5), "=" * width, groups.get(i, 0))
+    print(str(i * GROUP_MS).rjust(5) + "ms", "=" * width, groups.get(i, 0))
 
 print()
 q = statistics.quantiles(groups.values(), n=100)
